f9191fe0a89fb0f69d2d05b6410ad462b93ae2f7
Artem Garanskyy <art.garanskiy@gmail.com>
2024-03-23 20:49:38 +0800

    order was added to the seed and model updated for allow a create method.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Order;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -18,6 +19,12 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+        Order::create([
+            'user_id'=>1,
+            'status'=>'processing',
+            'created_at'=>now(),
+            'updated_at'=>now(),
         ]);
     }
 }
